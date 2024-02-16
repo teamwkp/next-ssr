26f9bd0692f67c4d0ae09f4d6a7821a93d1b0aa4
arsize <arsizeof3416@gmail.com>
2024-02-16 13:16:06 +0800

    feat: add nav page

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,12 +1,20 @@
 const Home = () => {
   return (
     <div className="media-container mx-auto">
-      <div className="bg-[#F2F2FA] w-full mt-[100px] h-[150px] rounded-2xl">
-        <div className="left">
-          <div>精彩导航 智享骑行</div>
+      <div className="bg-[#F2F2FA] w-full mt-[100px] h-[200px] flex items-center rounded-2xl relative">
+        <div className="left-[130px] w-1/3 absolute">
+          <div className="text-3xl font-medium tracking-wider text-center mb-[25px]">
+            精“彩”导航 智享骑行
+          </div>
+          <div className="sub-text text-center font-light text-sm leading-6">
+            <div>专注于以自行车码表为代表的运动智能硬件</div>
+            <div>让骑行变得更加轻松</div>
+            <div>服务于全球超百万骑行运动爱好者</div>
+          </div>
         </div>
         <div className="img"></div>
       </div>
+      <div className="video-container"></div>
     </div>
   )
 }
