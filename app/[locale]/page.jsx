6751e08083d0695b3d4dce7bd05cca33de6a4c84
zhangliapp <674937076@qq.com>
2024-02-18 16:37:29 +0800

    feat: 首页动画组件优化

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -26,10 +26,11 @@ const Home = () => {
           <img src="/top-banner.png" alt="" />
         </div>
       </div>
-      <div className="h-[742px] bg-no-repeat bg-center rounded-2xl mt-[48px] flex justify-center items-center" style={{ backgroundImage: "url('/video-bg.png')" }}>
-        <div className="space-y-[10px] text-center">
+      <div className="h-[742px] bg-no-repeat relative rounded-2xl mt-[48px] overflow-hidden">
+        <video className="w-full h-full object-fill" autoPlay src="https://xiaoyueyue.com.cn/video.mp4" muted></video>
+        <div className="space-y-[10px] text-center absolute w-full h-full z-[99px] top-0 left-0 flex flex-col items-center justify-center text-white">
           <div className="font-bold text-[40px]">iGPSPORT</div>
-          <div className="text-[24px] text-[#0E0606]">记录精彩骑迹  为骑行运动而生</div>
+          <div className="text-[24px]">记录精彩骑迹  为骑行运动而生</div>
           <div className="pt-[20px]">
             <img className="mx-auto cursor-pointer" src="/play-video.png" alt="" />
           </div>
@@ -99,12 +100,12 @@ const Home = () => {
               slidesPerView={1}
               autoplay={true}
               >
-              <SwiperSlide className="w-full">
+              <SwiperSlide className="w-full px-1">
                 <div>十年赛场征战，我对专业装备的要求近乎严苛，iGPSPORT的GPS码表已成为我最信任的骑行伙伴之一。</div>
                 <div className="mt-[30px]">- 孔文生</div>
                 <div className="mt-[5px] text-[12px]">知名男车手</div>
               </SwiperSlide>
-              <SwiperSlide className="w-full">
+              <SwiperSlide className="w-full px-1">
                 <div>十年赛场征战，我对专业装备的要求近乎严苛，iGPSPORT的GPS码表已成为我最信任的骑行伙伴之一。</div>
                 <div className="mt-[30px]">- 孔文生</div>
                 <div className="mt-[5px] text-[12px]">知名男车手</div>
@@ -117,17 +118,25 @@ const Home = () => {
           </div>
         </div>
         <div className="absolute w-[400px] h-[400px] right-[100px] z-10">
-          <div className="absolute size-[55px] bg-[#ccc] rounded-full  top-[35px] left-[55px] animate-spin ">
+          <div className="w-full h-full  avatar-animation">
+            <div className="absolute size-[55px] bg-[#ccc] rounded-full  top-[35px] left-[55px]">
+              <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
+            </div>
+            <div className="absolute size-[100px] bg-[#ccc] rounded-full top-[10px] left-[270px]  ">
+              <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
+            </div>
+            <div className="absolute size-[50px] bg-[#ccc] rounded-full top-[210px] left-[310px] ">
+              <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
+            </div>
+            <div className="absolute size-[60px] bg-[#ccc] rounded-full top-[310px] left-[230px] ">
+              <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
+            </div>
+            <div className="absolute size-[100px] bg-[#ccc] rounded-full top-[230px] left-[0px] ">
+              <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
+            </div>
           </div>
-          <div className="absolute size-[100px] bg-[#ccc] rounded-full top-[10px] left-[270px] animate-spin  ">
-          </div>
-          <div className="absolute size-[50px] bg-[#ccc] rounded-full top-[210px] left-[310px] animate-spin  ">
-          </div>
-          <div className="absolute size-[60px] bg-[#ccc] rounded-full top-[310px] left-[230px] animate-spin  ">
-          </div>
-          <div className="absolute size-[100px] bg-[#ccc] rounded-full top-[230px] left-[0px] animate-spin  ">
-          </div>
-          <div className="absolute size-[160px] bg-[#ccc] rounded-full top-[120px] left-[130px]">
+          <div className="absolute size-[160px] bg-[#ccc] rounded-full top-[120px] left-[130px] active-avatar-animation">
+            <img className="rounded-full w-full h-full" src="/sport-bg.png" alt="" />
           </div>
         </div>
       </div>
