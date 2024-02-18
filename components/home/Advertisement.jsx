90710390a05ae23f42d3f99197319573f5e791da
arsize <arsizeof3416@gmail.com>
2024-02-18 17:17:32 +0800

    feat: 调整swiper

--- components/home/Advertisement.jsx
+++ components/home/Advertisement.jsx
@@ -9,9 +9,24 @@ import Image from "next/image"
 
 function Advertisement() {
   const quotation = [
-    "十年赛场征战，我对专业装备的要求近乎严苛，iGPSPORT的GPS码表已成为我最信任的骑行伙伴之一。",
-    "哈哈哈哈哈哈哈",
-    "吼吼吼吼厚厚",
+    {
+      text: "十年赛场征战，我对专业装备的要求近乎严苛，iGPSPORT的GPS码表已成为我最信任的骑行伙伴之一。",
+      img: "/avt1.png",
+      position: "top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]",
+      size: "w-[365px] h-[365px]",
+    },
+    {
+      text: "十年赛场征战2。",
+      img: "/avt1.png",
+      position: "top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]",
+      size: "w-[365px] h-[365px]",
+    },
+    {
+      text: "十年赛场征战3。",
+      img: "/avt1.png",
+      position: "top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]",
+      size: "w-[365px] h-[365px]",
+    },
   ]
   return (
     <div
@@ -25,6 +40,9 @@ function Advertisement() {
         </div>
         <Swiper
           modules={[Autoplay, EffectFade]}
+          onSlideChange={() => {
+            console.log("show")
+          }}
           effect={"fade"}
           fadeEffect={{
             crossFade: true,
@@ -36,7 +54,7 @@ function Advertisement() {
         >
           {quotation.map((k, i) => (
             <SwiperSlide className="w-full h-[100px]" key={i}>
-              <div className="w-full block">{k}</div>
+              <div className="w-full block">{k.text}</div>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -47,7 +65,16 @@ function Advertisement() {
         </div>
       </div>
       <div className="absolute right-[145px] z-10">
-        <img className="size-[365px]" src="/avatar.png" />
+        {/* <img className="size-[365px]" src="/avatar.png" /> */}
+        <div className="w-[365px] h-[365px] bg-red-500 relative">
+          <Image
+            className=" absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+            src={"/avt1.png"}
+            width={50}
+            height={50}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   )
