b4aa4a64e9f98b9e33ea860f2ac80b704f63f406
arsize <arsizeof3416@gmail.com>
2024-03-02 18:30:44 +0800

    fix: 替换icon

--- app/[locale]/aftersales/page.jsx
+++ app/[locale]/aftersales/page.jsx
@@ -14,29 +14,31 @@ const Aftersales = async ({ params: { locale } }) => {
     {
       title: "码表",
       url: "/sale-1.png",
-      height: 92,
-      width: 62,
+
       path: "/aftersales/codeTable",
     },
     {
       title: "配件",
       url: "/sale-2.png",
-      height: 92,
-      width: 100,
+
       path: "/aftersales/module",
     },
     {
       title: "应用",
       url: "/sale-3.png",
-      height: 92,
-      width: 122,
+
       path: "/aftersales/application",
     },
     {
-      title: "支持",
+      title: "渠道",
       url: "/sale-4.png",
-      height: 92,
-      width: 80,
+
+      path: "/aftersales/support",
+    },
+    {
+      title: "支持",
+      url: "/sale-5.png",
+
       path: "/aftersales/support",
     },
   ]
@@ -47,14 +49,14 @@ const Aftersales = async ({ params: { locale } }) => {
       resources={resources}
     >
       <Navbar bg={"transparent"} color={"#fff"} />
-      <div
-        className="w-full bg-no-repeat"
-        style={{
-          backgroundImage: "url('/sale-bg.png')",
-          backgroundSize: "100% auto",
-        }}
-      >
-        <div className="lg:h-[480px] lg:bg-[transparent] bg-[#111] h-[240px] flex justify-center items-center text-center">
+      <div className="w-full">
+        <div
+          style={{
+            backgroundImage: "url('/sale-bg.png')",
+            backgroundSize: "100% auto",
+          }}
+          className="bg-no-repeat lg:h-[480px] lg:bg-[transparent] bg-[#111] h-[240px] flex justify-center items-center text-center"
+        >
           <div className="lg:w-[640px] w-3/4">
             <div className="text-[42px] text-white tracking-[2px]">
               使用帮助
@@ -77,17 +79,17 @@ const Aftersales = async ({ params: { locale } }) => {
           </div>
         </div>
         <div className="media-container mx-auto pb-20 text-center">
-          <div className="text-[36px] mt-[80px]">售后模块</div>
+          <div className="text-[36px] mt-[90px] font-bold">售后模块</div>
           <div className="py-[24px]">选择对应的版块，提供具体的产品支持</div>
-          <div className="grid lg:grid-cols-4 grid-cols-2 gap-[20px]">
+          <div className="grid lg:grid-cols-5 grid-cols-2 gap-[20px]">
             {saleList.map((item) => {
               return (
                 <Link href={item.path}>
-                  <div className="lg:h-[264px] h-[200px] bg-[#F3F4F8] rounded-2xl flex flex-col justify-center items-center">
+                  <div className="h-[200px] w-[200px] bg-[#F3F4F8] rounded-2xl flex flex-col justify-center items-center">
                     <Image
                       className="transition-transform hover:scale-110 cursor-pointer"
-                      height={item.height}
-                      width={item.width}
+                      height="80"
+                      width="80"
                       src={item.url}
                       alt=""
                     ></Image>
